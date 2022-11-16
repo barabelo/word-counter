08f8a44a0cce23addad6d8543f2ad78e03812f78
Bruno Almeida Rabelo <brunoalmeida@gec.inatel.br>
2022-11-15 21:16:50 -0300

    Implementação da classe principal.

--- src/main/java/br/inatel/Main.java
+++ src/main/java/br/inatel/Main.java
@@ -1,7 +1,39 @@
 package br.inatel;
 
-public class Main {
-    public static void main(String[] args) {
+import br.inatel.model.Frase;
+import br.inatel.observadores.implementacoes.PalavrasIniMaiusculas;
+import br.inatel.observadores.implementacoes.PalavrasPares;
+import br.inatel.observadores.implementacoes.TotalDePalavras;
+import br.inatel.observaveis.FraseObservavel;
 
+import java.util.Scanner;
+
+public class Main {
+    private static final FraseObservavel fraseObservavel = new FraseObservavel();
+    private static final TotalDePalavras totalDePalavras = new TotalDePalavras();
+    private static final PalavrasPares palavrasPares = new PalavrasPares();
+    private static final PalavrasIniMaiusculas palavrasIniMaiusculas = new PalavrasIniMaiusculas();
+
+    public static void main(String[] args) {
+        setUp();
+        String txt;
+        do {
+            System.out.println("Digite uma frase ou ++s para sair: ");
+            Scanner sc = new Scanner(System.in);
+            txt = sc.nextLine();
+            if (txt.length() > 0) {
+                if (!txt.equals("++s")) {
+                    fraseObservavel.setFrase(new Frase(txt));
+                    fraseObservavel.notificaObservadores();
+                }
+            }
+        } while (!txt.equals("++s"));
     }
+
+    private static void setUp() {
+        fraseObservavel.registraObservador(totalDePalavras);
+        fraseObservavel.registraObservador(palavrasPares);
+        fraseObservavel.registraObservador(palavrasIniMaiusculas);
+    }
+
 }
